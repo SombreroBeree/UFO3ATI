1d044d9a54dab5d6ed309a6fc5b3a9e470e914eb
Bereee <m.black.wood.white@gmail.com>
2023-05-22 22:34:44 +0200

    nowy projekt według wytycznych które mi powiedziano
    Nowe repozytorium z całym nowym projektem na raz bo nie chciało mi się bawić z poprzednimi wersjami

---
+++ ufo/Assets/Scripts/AudioManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public Sound[] music;
+    public Sound[] sfx;
+    public AudioSource musicSource;
+    public AudioSource sfxSource;
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
